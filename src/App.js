eba965ba1930d25973289c29449e533ef9217df5
Mace Akkari <mace.akkari@relyfe.com>
2023-02-13 13:09:05 +0000

    basic features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div>
+          <form>
+            <input
+              className="input"
+              value={""}
+              placeholder="Enter Github username"
+            />
+            <button className="button">Click me</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
